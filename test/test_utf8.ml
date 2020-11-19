0150b2e028682f891033cbed235b3fc44389788e
Alan Hu <alanh@ccs.neu.edu>
2020-11-19 08:47:02 -0500

    Test match_ for UTF-8

--- test/test_utf8.ml
+++ test/test_utf8.ml
@@ -1,4 +1,5 @@
 let test_search = Test_util.test_search Oniguruma.Encoding.utf8
+let test_match = Test_util.test_match Oniguruma.Encoding.utf8
 
 let () =
   test_search "a|b" "a" [0, 1];
@@ -6,4 +7,5 @@ let () =
   test_search "あ*" "" [0, 0];
   test_search "あ*" "あ" [0, 3];
   test_search "あ|a" "a" [0, 1];
-  test_search "あ|a" "あ" [0, 3]
+  test_search "あ|a" "あ" [0, 3];
+  test_match "a" 3 "あa" [3, 4]
